249c5d084a362274f74e1d1a7498f13764210478
SallyKimOh <ssang_000@Sally>
2016-10-23 13:08:36 -0400

    Assign1

--- Test/src/Assign1.java
+++ Test/src/Assign1.java
@@ -1,9 +1,8 @@
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * 
- */
 
 /**
  * @author Sae il Kim
@@ -15,7 +14,7 @@ import java.util.Scanner;
  * @Create_date 2016. 9. 19.
  *
  * @Modify_User
- * @Modify_date 2016. 9. 19.
+ * @Modify_date 2016. 9. 24.
  */
 public class Assign1 {
 
@@ -25,9 +24,12 @@ public class Assign1 {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
+		DecimalFormat form = new DecimalFormat("#.##");
+
+		Scanner info = new Scanner(System.in);
+		
 		try {
 		
-			Scanner info = new Scanner(System.in);
 			int dolM = 0;	//dollars
 			int cM=0;		//cents
 			
@@ -36,112 +38,74 @@ public class Assign1 {
 			int qM = 0;		//Quarters
 			int dM = 0;		//Dimes
 			int nM = 0;		//Nickels
-			String sPanny = "0";
+			
 			
 			System.out.println("Change is good");
-			System.out.print("How many dollars? ");
-	
-			String sDolM = info.nextLine();
 			
+			//=================== Exception start ===================================//
+
 			while (true) {
 
 				try {
-					dolM = Integer.parseInt(sDolM);
-					if (dolM < 0 ) {
-						System.out.println("- is not dollars. You have to put in the dollars.");
-						System.out.print("How many dollars? ");
-						sDolM = info.nextLine();
+					System.out.print("How many dollars? ");
+					dolM = info.nextInt();
+					System.out.print("How many cents? ");
+					cM = info.nextInt();
+					if ((dolM < 0 ) || (cM < 0 )) {
+						System.out.println("This is not real Money. Can you put in the Money again?");
 					} else {
 						break;
 					}
 					
-				} catch (Exception e1) {
-					System.out.println("This is not dollars. You have to put in the dollars.");
-					System.out.print("How many dollars? ");
-					sDolM = info.nextLine();
-					
-				}
-
-			}
-
-			System.out.print("\nHow many cents? ");
-
-			String sTcM = info.nextLine();
-			
-			while (true) {
-
-				try {
-					cM = Integer.parseInt(sTcM);
-					if (cM < 0 ) {
-						System.out.println("- is not cents. You have to put in the cents.");
-						System.out.print("How many cents? ");
-						sTcM = info.nextLine();
-					} else {
-						break;
-					}
-					
-				} catch (Exception e1) {
-					System.out.println("This is not cents. You have to put in the cents.");
-					System.out.print("How many dollars? ");
-					sTcM = info.nextLine();
-					
+				} catch (InputMismatchException e1) {
+					System.out.println("This is not real Money.  You have to put in the number for Money.");
+					info.close();
+					return;
 				}
 
 			}
 			
+			//=================== Exception end ===================================//
 			
-			if (String.valueOf(cM).length() > 1) {
-				sPanny = String.valueOf(cM).substring(String.valueOf(cM).length()-1,String.valueOf(cM).length());
+			
+			
+			//=================== Real Calculate start ===================================//
 
-				if (Integer.parseInt(sPanny) <= 2 ) {
-					cM = Integer.parseInt(String.valueOf(cM).substring(0,String.valueOf(cM).length()-1)+"0");
-				} else if ((Integer.parseInt(sPanny) > 2 ) && ( Integer.parseInt(sPanny) <= 7 )) {
-					cM = Integer.parseInt(String.valueOf(cM).substring(0,String.valueOf(cM).length()-1)+"5");
-				} else if (Integer.parseInt(sPanny) > 7 ) {
-					cM = Integer.parseInt(String.valueOf(cM).substring(0,String.valueOf(cM).length()-1)+"5")+5;
-				}
-				
-					
-				if (cM > 100) {
-					dolM += Integer.parseInt(String.valueOf(cM).substring(0,String.valueOf(cM).length()-2));
+			int total = ((dolM*100)+cM) ;
 
-					String sCM = ""+cM;
-
-					cM = Integer.parseInt(sCM.substring(String.valueOf(cM).length()-2,String.valueOf(cM).length()));
-
-				}
-					
-			} else {
-				if (cM <= 2 ) {
-					cM = 0;
-				} else if ((cM > 2 ) && ( cM <= 7 )) {
-					cM = 5;
-				} else if (cM > 7 ) {
-					cM = 10;
-				}
-				
-					
-			}
-				    
+			double d = total/100;
+			
+			double c = Double.parseDouble(form.format(total%100*0.01));
+			
+			double reMoney = d+c;
+			
+			dolM += (cM+2)/100;
+			cM = (cM+2) % 100;
+			
 			tM = dolM / 2;  
 			lM = dolM % 2;
 			qM = cM / 25;
 			dM = cM % 25 / 10;
 			nM = cM % 25 % 10 /5;
-				
-			System.out.println("$"+ dolM + "."+sTcM +" requires: \n"); 
-			System.out.println("Toonies:"+ tM); 
-			System.out.println("Loonies:"+ lM); 
-			System.out.println("Quarters:"+ qM);
-			System.out.println("Dimes:"+ dM);
-			System.out.println("Nickels:"+ nM);
-				
-			info.close();
-				
+
+
+			System.out.println("\n$"+ reMoney +" requires:"); 
+			
+			System.out.println(tM + " Toonies"); 
+			System.out.println(lM + " Loonies"); 
+			System.out.println(qM + " Quarters");
+			System.out.println(dM + " Dimes");
+			System.out.println(nM + " Nickels");
+			
+			
+			//=================== Real Calculate end ===================================//
+			
+			
 		
 		} catch (Exception e) {
-			System.out.println("error==>"+e);
-			
+			System.out.println("It happens the critical problem.");
+			info.close();
+		
 		}
 	}
 
